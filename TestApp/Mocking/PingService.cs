4032bfdaa3598ca80e38f78339b512be16597ee4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-19 09:15:24 +0200

    Initial commit

---
+++ TestApp/Mocking/PingService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Mocking
+{
+
+
+   
+    public class InstallerHelper
+    {
+        private string _setupDestinationFile;
+
+        public bool DownloadInstaller(string customerName, string installerName)
+        {
+            var client = new WebClient();
+            try
+            {
+                client.DownloadFile(
+                    string.Format("http://example.com/{0}/{1}",
+                        customerName,
+                        installerName),
+                    _setupDestinationFile);
+
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+    }
+}
