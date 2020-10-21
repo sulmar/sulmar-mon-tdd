4d5018b55f640d01f19c8de077743dad3043d516
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-21 08:34:05 +0200

    OrderSender

---
+++ TestApp/Mocking/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Mocking
+{
+    public class OrderSender
+    {
+        public void Send(Order order)
+        {
+            decimal shippingCost = CalculateShippingCost(order);
+
+            SendEmail($"Koszt dostawy {shippingCost}");
+        }
+
+        private void SendEmail(string messsage)
+        {
+            Console.WriteLine($"Sending {messsage}...");
+        }
+
+        private decimal CalculateShippingCost(Order order)
+        {
+            if (order.Total > 1000)
+                return 0;
+            else
+                return 9.99m;
+        }
+    }
+
+}
